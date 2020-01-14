e3319ca436ae4d50fc572b0ad1b8699167a2372b
Laurentiu Ciobanu | vex <laurentiu.ciobanu@datastacker.dev>
2020-01-14 18:01:54 +0200

    Added basic TypeDi integration under @arck/core/di

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -1,2 +1,30 @@
 import arck from "@arck/core";
+
+import Container, { Service, Inject } from "@arck/core/di";
+
 import "./test";
+
+Container.set("test.key", "bla bla");
+
+@Service()
+class TestService {
+    @Inject("test.key") private key: string;
+
+    public test() {
+        console.log(`test key: ${this.key}`);
+    }
+}
+
+@Service()
+class DemoService {
+    constructor(
+        private testService: TestService
+    ) {
+    }
+
+    public demo() {
+        this.testService.test();
+    }
+}
+
+Container.get(DemoService).demo();
